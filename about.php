0fb3a7ffdcf62d77dd125dfa82ba71e582837f2f
Koder AB <kumariastha003@gmail.com>
2021-07-30 18:41:57 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	<script type="text/javascript" src="bootstrap/js/jquery-3.5.1.min.js"></script>
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script> 
+
+</head>
+<body>
+		<div>
+			<H4>ABOUT THIS APP</H4><br>
+			<img src="images/1.png" class="img-responsive" style="height: 200px" >
+			<p class = "text-justify">P Droid is an productivity tool which helps you to stay productive by keeping a track of your time of all your works and by keeping motivated to work by planting a focus plant , and it is having a auto reply bot for your WhatsApp ,Facebook messenger ,so that when your busy switch on the bot to reply to your incoming messages.</p>
+	    </div>
+
+
+		<div>
+			<H4>HOW IT WORKS ?</H4><br>
+			<p class="text-justify">Add your tasks , plant a tree when you need to stay focused .if you commit to your work without distracting ,your tree grows ,if  you cannot resist the temptation of using your phone, your trees dies and over a time you will built an entire forest from your focus and commitment and in between if you have any incoming messages bot will auto reply ,but please note P droid auto reply lies on WhatsApp and Facebook messengers notifications for auto reply to work. Most users usually have notifications enabled so should work without any additional configuration but otherwise please make sure notifications are enabled and WhatsApp fingerprint lock is disabled for this app to work. </p>
+			</div>
+
+
+	    <div>
+			<H4>FEATURES</H4><br>
+		(1.) Respect privacy-we do not collect a single data everything is stored in your local database.<br>
+		(2.) Smart reply – Set Continuous auto reply or set a reply frequency.<br>
+        (3.) Group Support-it support group message to WhatsApp to reply any WhatsApp group.<br>
+        (4.) Simple Turn on/off-turn on/off with single touch switch.<br>
+        (5.) Contact matters- chose your desires contact to whom you want to reply.<br>
+        (6.) React to all- when your quite busy reply to all message.<br>
+        (7.) Custom message – you can set your custom message.<br>
+        (8.) Task management- Task organiser, time tracker.<br>
+        (9.) Focus to do- complete your study, work, homework.<br>
+        (10.) Statistics – get chart of all your time spent in a week.<br>
+        (11.) Work target- set target and track how much you have completed.<br>
+        (12.) Revision timer – student can set a revision time to have a dedicated slot for revision.<br>
+        (13.) Reduce phone addiction-a unique app to beat phone addictions.<br>
+        (14.) Stay focused- by planting a focus tree.<br>
+        (15.) Records – a list of records of time spent.<br>
+        
+        </div>
+	</div>	
+   		
+
+ 
+		
+	     
